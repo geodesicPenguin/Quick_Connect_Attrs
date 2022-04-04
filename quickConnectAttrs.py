646362fbbbcf11de075c4481f44ead0cacc3a8b3
geodesicPenguin <lms2k@live.com>
2022-04-04 15:52:10 -0400

    Update quickConnectAttrs.py

--- quickConnectAttrs.py
+++ quickConnectAttrs.py
@@ -14,6 +14,32 @@ def ATTRIBUTE_UI():
     '''
 
     # Create window object
+    mainWin = 'attr_ui_main'
+    if pm.window(mainWin, exists=1):
+        pm.deleteUI(mainWin)
+
+    with pm.window(mainWin,resizeToFitChildren=1,mainMenuBar=1,title='Connect Attributes') as mw:
+
+        # Diplsays selected objects and allows the user to change source and destination inputs
+        with pm.rowLayout():
+            pm.textFieldButtonGrp('source_input',text='Source: ') # SOURCE: input is first object in selection index. Button updates input with selected object -- FAILS if more than 1 object selected
+        with pm.rowLayout():
+            pm.text(label='Destination: ')
+            pm.textScrollList('dest_input',allowMultiSelection=1,append=# default selected items inexed after 0)
+            pm.button('refresh_dest',label="Refresh") # refreshes list to what is selected 
+            pm.button('remove_object',label="Remove") # removes object from list
+        pm.separator(height=40,style='in')
+
+        # Displays attributes of source object and destination attributes shared by all destination objects
+        with pm.rowLayout():
+            with pm.columnLayout():
+                pm.text(label='Source Attributes')
+                pm.textScrollList('source_attrs',append=#list of attrs on source obj -- UPDATE REG)
+            with pm.columnlayout():
+                pm.text(label='Destination Attributes')
+                pm.textScrollList('dest_attrs',append=#dest attrs that are the same -- UPDATE REG)
+
+
 
 
     # Add UI objects
