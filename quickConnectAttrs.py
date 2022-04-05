9284825754b8fc164dd560dec9efc92e564cfa94
geodesicPenguin <lms2k@live.com>
2022-04-04 23:03:11 -0400

    UI updates
    changed the UI set up a little

--- quickConnectAttrs.py
+++ quickConnectAttrs.py
@@ -18,26 +18,44 @@ def ATTRIBUTE_UI():
     if pm.window(mainWin, exists=1):
         pm.deleteUI(mainWin)
 
-    with pm.window(mainWin,resizeToFitChildren=1,mainMenuBar=1,title='Connect Attributes') as mw:
+    # General UI values    
+    menuItemNum=10 # custom default UI row item limit 
+    w = 100 #width
+    with pm.window(mainWin,resizeToFitChildren=1,title='Connect Attributes'):
+        
+        with pm.columnLayout():
 
         # Diplsays selected objects and allows the user to change source and destination inputs
-        with pm.rowLayout():
-            pm.textFieldButtonGrp('source_input',text='Source: ') # SOURCE: input is first object in selection index. Button updates input with selected object -- FAILS if more than 1 object selected
-        with pm.rowLayout():
-            pm.text(label='Destination: ')
-            pm.textScrollList('dest_input',allowMultiSelection=1,append=# default selected items inexed after 0)
-            pm.button('refresh_dest',label="Refresh") # refreshes list to what is selected 
-            pm.button('remove_object',label="Remove") # removes object from list
-        pm.separator(height=40,style='in')
+            with pm.rowLayout(numberOfColumns=menuItemNum):
+                #pm.textFieldButtonGrp('source_input',label='Source: ') # SOURCE: input is first object in selection index. Button updates input with selected object -- FAILS if more than 1 object selected
+                pm.text(label='Source: ',width=w)
+                pm.textField('source_input',width=w*2)
+                pm.button('refresh_source',label='Refresh',width=w)
+            with pm.rowLayout(numberOfColumns=menuItemNum):
+                pm.separator(height=10,width=w*5,style='none')
+            with pm.rowLayout(numberOfColumns=menuItemNum):
+                pm.text(label='Destination: ',width=w)
+                pm.textScrollList('dest_input',allowMultiSelection=1,append=None,width=w*2)# default selected items inexed after 0)
+                pm.button('refresh_dest',label="Refresh") # refreshes list to what is selected 
+                pm.button('remove_object',label="Remove") # removes object from list
+            with pm.rowLayout(numberOfColumns=menuItemNum):
+                pm.separator(height=10,width=w*5,style='in')
+        
+            # Displays attributes of source object and destination attributes shared by all destination objects
+            with pm.rowLayout(numberOfColumns=menuItemNum):
+                with pm.columnLayout():
+                    pm.text(label='Source Attributes')
+                    pm.textScrollList('source_attrs',append=None)#list of attrs on source obj -- UPDATE REG)
+                with pm.columnLayout():
+                    pm.text(label='Destination Attributes')
+                    pm.textScrollList('dest_attrs',append=None)#dest attrs that are the same -- UPDATE REG)
+        
+            # Execute or cancel
+            with pm.rowLayout(numberOfColumns=menuItemNum):
+                pm.button('execute_button',label='OK')
+                pm.button('cancel_button',label='Cancel')
 
-        # Displays attributes of source object and destination attributes shared by all destination objects
-        with pm.rowLayout():
-            with pm.columnLayout():
-                pm.text(label='Source Attributes')
-                pm.textScrollList('source_attrs',append=#list of attrs on source obj -- UPDATE REG)
-            with pm.columnlayout():
-                pm.text(label='Destination Attributes')
-                pm.textScrollList('dest_attrs',append=#dest attrs that are the same -- UPDATE REG)
+
 
 
 
@@ -55,6 +73,7 @@ def SELECTION_QUERY():
 
     returns list of selections [OUTGOING,[INCOMING]]
     '''
+    pass
 
     # outgoing connection
 
@@ -68,6 +87,7 @@ def ATTRIBUTE_QUERY(selection):
     
     returns (dict?)list of all available attributes for connection
     '''
+    pass
 
 def CONNECTION_TYPE_QUERY():
     '''
@@ -78,4 +98,5 @@ def CONNECT_ATTRIBUTES(attributes):
     '''
     Connects attributes.
     '''
+    pass
 
